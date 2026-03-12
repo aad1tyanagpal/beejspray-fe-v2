f84d081dc79275851229986dd624693b1814a75c
aad1tyanagpal <nagpal.aaditya@gmail.com>
2026-03-13 02:36:48 +0530

    Lets see is removing browser roustes will work

--- src/apps/seller/main.jsx
+++ src/apps/seller/main.jsx
@@ -9,9 +9,9 @@ import '@/index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
         <App />
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   </StrictMode>
 )
--- src/apps/seller/routes/SellerRoutes.jsx
+++ src/apps/seller/routes/SellerRoutes.jsx
@@ -1,22 +1,16 @@
-import {  Routes, Route } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Profile from "../pages/Profile";
-
 import ProtectedRoute from "./ProtectedRoute";
 import Dashboard from "../pages/Dashboard";
 import Inventory from "../pages/Inventory";
 import SellerLayout from "../layout/SellerLayout";
 
 const SellerRoutes = () => {
-  const base = import.meta.env.BASE_URL || '/';
   return (
     <Routes>
       <Route element={<SellerLayout />}>
-        {/* Public Routes */}
         <Route path="/" element={<Dashboard />} />
         <Route path="/inventory" element={<Inventory />} />
-
-        {/* Private Routes */}
         <Route element={<ProtectedRoute />}>
           <Route path="/profile" element={<Profile />} />
         </Route>
